cc00adf88b6736d3143f5a44bfa823c72f9f005a
anny_more <anxxxen@ya.ru>
2022-05-02 15:09:56 +0300

    feat: I DID IT!

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(cond = true) {
+    this.isReverse = cond;
+    this.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  };
+  
+  encrypt(str, kod) {
+    if (arguments.length < 2 || (typeof(str) !== 'string') || (typeof(kod) !== 'string')) {
+      throw new Error('Incorrect arguments!');
+    };
+    const array = str.split('');
+    let key = "";
+    while (key.length < array.length) {
+      key = key + kod;
+    }
+    key = key.split('').map(item => item = this.alphabet.indexOf(item.toUpperCase()) >= 0 ? this.alphabet.indexOf(item.toUpperCase()) : item);
+    key.length = array.length;
+    let letterKod = array.map(item => 
+      item = this.alphabet.indexOf(item.toUpperCase()) >= 0 ? this.alphabet.indexOf(item.toUpperCase()) : item);
+    
+    let i = 0;
+    let lKod = letterKod.map((item, index) => {
+      if (typeof(item) === 'number') {
+        return item = this.alphabet[item + key[index - i] > 25 ? item + key[index - i] - 26 : item + key[index - i]];
+      } else {
+        i++;
+        return item = item;
+      }
+    });
+    if (this.isReverse) {
+      return lKod.join('');
+    } else {
+      return lKod.reverse().join('');
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(str, kod) {
+    if (arguments.length < 2 || (typeof(str) !== 'string') || (typeof(kod) !== 'string')) {
+      throw new Error('Incorrect arguments!');
+    }
+    const array = str.split('');
+    let key = "";
+    while (key.length < array.length) {
+      key = key + kod;
+    }
+    key = key.split('').map(item => item = this.alphabet.indexOf(item.toUpperCase()) >= 0 ? this.alphabet.indexOf(item.toUpperCase()) : item);
+    key.length = array.length;
+    let letterKod = array.map(item => 
+      item = this.alphabet.indexOf(item.toUpperCase()) >= 0 ? this.alphabet.indexOf(item.toUpperCase()) : item);
+    
+    let i = 0;
+    let lKod = letterKod.map((item, index) => {
+      if (typeof(item) === 'number') {
+        return item = this.alphabet[(item - key[index - i]) < 0 ? (26 + item - key[index - i]) : (item - key[index - i])];
+      } else {
+        i++;
+        return item = item;
+      }
+    });
+    if (this.isReverse) {
+      return lKod.join('');
+    } else {
+      return lKod.reverse().join('');
+    }
   }
 }
 
